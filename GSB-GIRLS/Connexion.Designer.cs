f38c63740b685784a44ce1e34f7155c22b7c2c80
SIO <SIO@ST202>
2017-11-06 16:49:31 +0100

    connect

--- GSB-GIRLS/Connexion.Designer.cs
+++ GSB-GIRLS/Connexion.Designer.cs
@@ -34,27 +34,30 @@
             this.bsvisiteurs = new System.Windows.Forms.BindingSource(this.components);
             this.valider = new System.Windows.Forms.Button();
             this.quitter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.bsvisiteurs)).BeginInit();
             this.SuspendLayout();
             // 
             // txtIdent
             // 
-            this.txtIdent.Location = new System.Drawing.Point(100, 93);
+            this.txtIdent.Location = new System.Drawing.Point(288, 136);
             this.txtIdent.Name = "txtIdent";
-            this.txtIdent.Size = new System.Drawing.Size(307, 20);
+            this.txtIdent.Size = new System.Drawing.Size(300, 20);
             this.txtIdent.TabIndex = 1;
             // 
             // textMPD
             // 
-            this.textMPD.Location = new System.Drawing.Point(100, 180);
+            this.textMPD.Location = new System.Drawing.Point(288, 184);
             this.textMPD.Name = "textMPD";
-            this.textMPD.Size = new System.Drawing.Size(307, 20);
+            this.textMPD.Size = new System.Drawing.Size(300, 20);
             this.textMPD.TabIndex = 2;
             // 
             // valider
             // 
             this.valider.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.valider.Location = new System.Drawing.Point(157, 278);
+            this.valider.Location = new System.Drawing.Point(304, 278);
             this.valider.Name = "valider";
             this.valider.Size = new System.Drawing.Size(75, 23);
             this.valider.TabIndex = 3;
@@ -65,7 +68,7 @@
             // quitter
             // 
             this.quitter.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.quitter.Location = new System.Drawing.Point(272, 278);
+            this.quitter.Location = new System.Drawing.Point(477, 278);
             this.quitter.Name = "quitter";
             this.quitter.Size = new System.Drawing.Size(75, 23);
             this.quitter.TabIndex = 4;
@@ -73,10 +76,47 @@
             this.quitter.UseVisualStyleBackColor = false;
             this.quitter.Click += new System.EventHandler(this.quitter_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(281, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Identification";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(91, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Identifiant :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(91, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mot de passe :";
+            // 
             // Connexion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(823, 413);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.quitter);
             this.Controls.Add(this.valider);
             this.Controls.Add(this.textMPD);
@@ -86,6 +126,9 @@
             this.Controls.SetChildIndex(this.textMPD, 0);
             this.Controls.SetChildIndex(this.valider, 0);
             this.Controls.SetChildIndex(this.quitter, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
             ((System.ComponentModel.ISupportInitialize)(this.bsvisiteurs)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -98,6 +141,9 @@
         private System.Windows.Forms.BindingSource bsvisiteurs;
         private System.Windows.Forms.Button valider;
         private System.Windows.Forms.Button quitter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
